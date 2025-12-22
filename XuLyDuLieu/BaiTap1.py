3b0cb6d9f77e24c1665de5b2cd669e811dfa122f
Thai Nhan <thainhan07103@gmail.com>
2025-12-22 10:01:12 +0700

    Add bài

---
+++ XuLyDuLieu/BaiTap1.py
@@ -0,0 +1,17 @@
+import numpy as np  
+import matplotlib.pyplot as plt
+
+text_file = open("XuLyDuLieu/ex1data1.txt", "r")
+lines = text_file.readlines()
+
+data = np.array([[float(x) for x in line.split(",")] for line in lines])
+text_file.close()
+
+m, n = data.shape
+
+X = data[:, range(n-1)]
+Y = data[:, -1]
+
+X = np.insert(X, 0, 1, axis=1)
+
+print(X)
